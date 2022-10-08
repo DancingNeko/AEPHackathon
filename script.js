82039cde5e5e4ef6cf0ac74e09ee237177ddf28c
Daniel Feng <daniel@Daniels-MacBook-Air-3.local>
2022-10-08 16:23:03 -0400

    update

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 //let canvas = document.getElementById("draw");
 
-var dataStr;
+
 
 const fileInput = document.getElementById('csv')
 const readFile = () => {
   const reader = new FileReader()
   reader.onload = () => {
-    //document.getElementById('out').innerHTML = reader.result
-    dataStr = reader.result;
-    mat = str2matrix(dataStr)
-    flipped = mat[0].map((_, colIndex) => mat.map(row => row[colIndex]));
-    const xPoints = flipped[128].slice(2).map(Number);
-    const yPoints = flipped[30].slice(2).map(Number);
-    plotData(xPoints,yPoints);
+    var xDatas = [];
+    var yDatas = [];
+    alert(fileInput.files.length)
+    for(i = 0; i < fileInput.files.length; i++){
+        reader.readAsBinaryString(fileInput.files[i]);
+        dataStr = reader.result;
+        mat = str2matrix(dataStr[i]);
+        flipped = mat[0].map((_, colIndex) => mat.map(row => row[colIndex]));
+        var windSpeed = flipped[127].slice(2).map(Number);
+        var windDirection = flipped[124].slice(2).map(Number);
+        var expectedPower = flipped[29].slice(2).map(Number);
+        var actualPower = flipped[69].slice(2).map(Number);
+        //const airDensity = flipped[4].slice(2).map(Number);
+        var ambientAirTemp = flipped[88].slice(2).map(Number);
+        var generatorInboardTemp = flipped[89].slice(2).map(Number);
+        var generatorOutboardTemp = flipped[90].slice(2).map(Number);
+        var rotationSpeed = flipped[59].slice(2).map(Number);
+        //const driveTrainAcceleration = flipped[27].slice(2).map(Number);
+        //const rotorSpeed = flipped[83].slice(2).map(Number);
+        //var offset = [];
+        //for (i = 0; i < expectedPower.length; i++) {
+        //    offset.push((expectedPower[i] - actualPower[i])/ expectedPower[i]);
+        //}
+        xDatas.push(rotationSpeed);
+        yDatas.push(actualPower);
+    }
+    plotData(xDatas,yDatas);
+    
     document.getElementById("out").innerHTML = "done";
   }
   // start reading the file. When it is done, calls the onload event defined above.
-    reader.readAsBinaryString(fileInput.files[0]);
 }
 fileInput.addEventListener('change', readFile)
 
@@ -30,8 +50,12 @@ function str2matrix(dataStr){
 }
 
 class Windmill {
-    constructor(diameter) {
-        rotorArea = Math.PI * Math.pow((diameter / 2),2);
+    diameter = 127;
+    hubHeight = 90;
+    rotorArea = Math.PI * Math.pow((diameter / 2),2);
+
+    constructor() {
+        
     }
     get rotorArea () {
         return rotorArea;
@@ -45,45 +69,20 @@ class Windmill {
     
 }
 
-function plotData(xPoints, yPoints) {
-    // Create an XY Plotter
-    let myPlotter = new XYPlotter("draw");
 
-    numPoints = xPoints.length;
-    // Plot the Points
-    myPlotter.plotPoints(numPoints, xPoints, yPoints, "blue");
+function plotData(xData, yData) {
+    var line;
+    var data = [];
+    for (i = 0; i < xData.length; i++) {
+        line = {
+            x: xData[i],
+            y: yData[i],
+            opacity: 0.5,
+            mode: "markers",
+            type: "scatter"
+        };
+        data.push(line);
+    }
+    Plotly.newPlot("scatter", data);
+}
 
-    // Plotter Object
-    function XYPlotter(id) {
-        this.canvas = document.getElementById(id);
-        this.ctx = this.canvas.getContext("2d");
-        this.xMin = 0;
-        this.yMin = 0;
-        this.xMax = this.canvas.width;
-        this.yMax = this.canvas.height;
-
-        // Plot Points Function
-        this.plotPoints = function(n, xArr, yArr, color, radius = 3) {
-            yMaxVal = 0;
-            yArr.forEach(val => {
-                yMaxVal = val > yMaxVal? val : yMaxVal;
-            });
-            xMaxVal = 0;
-            xArr.forEach(val => {
-                xMaxVal = val > xMaxVal? val : xMaxVal;
-            });
-            alert(xMaxVal);
-
-            
-
-            for (let i = 0; i < n; i++) {
-                this.ctx.fillStyle = color;
-                this.ctx.beginPath();
-                this.ctx.ellipse(xArr[i] * this.xMax / xMaxVal, yArr[i] * this.yMax / yMaxVal, radius, radius, 0, 0, Math.PI * 2);
-                this.ctx.fill();
-            }
-        }
-
-    } 
-}// End Plotter Object
-    
